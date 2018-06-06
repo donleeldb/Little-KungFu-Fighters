83f86ae90b4f34edd0cc13f8bd08ba80dce7cbe6
Ian Lee <ian.lee@berkeley.edu>
2018-06-06 01:08:55 -0700

    added original assets. Initial player movement done

---
+++ Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour {
+
+	public Transform lookAt;
+	private Vector3 offset = new Vector3 (0, 0, -15.0f);
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = lookAt.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private float inputDirection_x;
+	private float inputDirection_z;
+
+	private float verticalVelocity;
+	private float gravity = 1.0f;
+	private float speed = 5.0f;
+
+	private bool facingRight;
+
+	private Vector3 moveVector;
+	private Vector3 lastMotion;
+	private CharacterController controller;
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+		anim = GetComponentInChildren<Animator> ();
+		facingRight = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		moveVector = Vector3.zero;
+
+		inputDirection_x = Input.GetAxis ("Horizontal") * speed;
+		// TODO: change Vertical to sth else
+		inputDirection_z = Input.GetAxis ("Depth") * speed;
+
+		if (controller.isGrounded) { // not reliable
+//		if (IsControllerGrounded()) { // mine is bugged
+			Debug.Log ("grounded");
+			if (inputDirection_x == 0 && inputDirection_z == 0) {
+				anim.SetInteger ("State", 0);
+			} else {
+				anim.SetInteger ("State", 1);
+			}
+
+//			verticalVelocity = 0; // tutorial has this. But vertical velocity is reset in the else statement
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				verticalVelocity = 15;
+				anim.SetInteger ("State", 2);
+			}
+			moveVector.x = inputDirection_x;
+			moveVector.z = inputDirection_z;
+
+		} else {
+			Debug.Log ("not grounded");
+			verticalVelocity -= gravity;
+			moveVector.x = lastMotion.x;
+			moveVector.z = lastMotion.z;
+		}
+
+		print(verticalVelocity);
+		moveVector.y = verticalVelocity;
+		controller.Move (moveVector * Time.deltaTime);
+		Flip (moveVector.x);
+		lastMotion = moveVector;
+
+	}
+
+	private bool IsControllerGrounded() {
+
+		Vector3 leftRayStart;
+		Vector3 rightRayStart;
+
+		leftRayStart = controller.bounds.center;
+		rightRayStart = controller.bounds.center;
+
+		leftRayStart.x -= controller.bounds.extents.x;
+		rightRayStart.x += controller.bounds.extents.x;
+
+		Debug.DrawRay (leftRayStart, Vector3.down, Color.red);
+		Debug.DrawRay (rightRayStart, Vector3.down, Color.green);
+		Debug.Log (Physics.Raycast (leftRayStart, Vector3.down, (controller.height / 2) + 0.2f));
+		if (Physics.Raycast (leftRayStart, Vector3.down, (controller.height / 2) + 0.2f)) {
+			return true;
+
+		}
+		if (Physics.Raycast (rightRayStart, Vector3.down, (controller.height / 2) + 0.2f)) {
+			return true;
+		}
+		return false;
+	}
+
+	private void Flip(float speed) {
+		if (speed > 0 && !facingRight || speed < 0 && facingRight) {
+			facingRight = !facingRight;
+			Vector3 temp = transform.localScale;
+			temp.x *= -1;
+			transform.localScale = temp;
+		}
+	
+	}
+}
